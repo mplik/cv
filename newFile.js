1f159f73d9233b7548a74f4dc17c5133adac0cee
mplik <biuro@mplik.pl>
2025-02-26 18:02:44 +0100

    Usunięto niepotrzebny plik newFile.js

--- newFile.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('section h2').forEach(header => {
-    header.addEventListener('click', () => {
-        const section = this.parentElement;
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
